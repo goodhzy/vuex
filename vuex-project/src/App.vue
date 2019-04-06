bb63a1a3094588a7d44a26baa6368cb3b1d7d4ed
hzy <hzy@hzy.local>
2019-04-06 23:27:10 +0800

    vuex的简单小例子

---
+++ vuex-project/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <div>message:{{message}}</div>
+    <div>msg:{{msg}}</div>
+    <div class="btn" @click="onChange1">按钮1</div>
+    <div class="btn" @click="onChange2">按钮2</div>
+
+  </div>
+</template>
+
+<script>
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+const store = new Vuex.Store({
+  state:{
+    count: 0
+  },
+  mutations:{
+    increment(state){
+      state.count++
+    }
+  }
+})
+export default {
+  name: 'app',
+  data(){
+    return {
+      // 放在这里虽然状态改变了，但是不会重新赋值给msg，需在计算属性中响应
+      msg: this.$store.state.msg,
+    }
+  },
+  computed:{
+    message(){
+      return store.state.count
+    }
+  },
+  methods:{
+    onChange1(){
+      store.commit('increment')
+    },
+    onChange2(){
+      this.$store.commit('add')
+    },
+  }
+}
+</script>
+
+<style>
+  .btn{
+    background: blue;
+    padding: 10px;
+    width: 50px;
+    height: 20px;
+    margin-bottom: 20px;
+  }
+</style>
